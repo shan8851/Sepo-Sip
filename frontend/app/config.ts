c440c7b18dca77866f6a61d700fe4a62c3ab3115
shan8851 <asamshan456@gmail.com>
2024-09-19 21:24:53 +0100

    project id

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -4,13 +4,11 @@ import { http, createStorage, cookieStorage } from 'wagmi';
 import { mainnet, sepolia } from 'wagmi/chains';
 import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit';
 
-const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!;
-
 const supportedChains: Chain[] = [sepolia, mainnet];
 
 export const config = getDefaultConfig({
   appName: 'SepoSip',
-  projectId,
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   chains: supportedChains as any,
   ssr: true,
